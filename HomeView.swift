77147dc98fa179bd8dcbf2d6625224b17c5cfaa5
Brock Wrede <bee15dub@gmail.com>
2023-12-18 10:23:08 -0600

    added files

---
+++ HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Team Maddux Event App
+//
+//  Created by Brock R. Wrede on 12/18/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
