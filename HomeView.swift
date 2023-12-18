7d97e54a52ed38a3d3aa6e8e3cfe6260f979bb97
Tirth Patel <Tirthp0112@gmail.com>
2023-12-18 10:50:29 -0600

    vghbj

--- HomeView.swift
+++ HomeView.swift
@@ -2,56 +2,50 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
- 
-ZStack {
-    Color.lightBlue
-        .ignoresSafeArea()
-    
-    VStack {
-        Spacer()
-        
-        VStack {
-            
-            
+        NavigationView {
+            ZStack {
+                Color.lightBlue
+                    .ignoresSafeArea()
 
-                Text("TEAM MADDUX")
-                .font(.custom("Georgia", size: 32))
-                .fontWeight(.bold)
-                .padding()
-                .cornerRadius(10)
-                .foregroundColor(Color.black)
-            
-            NavigationLink(destination: AboutView()) {
-                Text("Maddux Albert Joseph - Life Story")
-                    .padding()
+                VStack {
+                    Spacer()
+
+                    VStack {
+                        Text("TEAM MADDUX")
+                            .font(.custom("Georgia", size: 32))
+                            .fontWeight(.bold)
+                            .padding()
+                            .cornerRadius(10)
+                            .foregroundColor(Color.black)
+
+                        NavigationLink(destination: AboutView()) {
+                            Text("Maddux Albert Joseph - Life Story")
+                                .padding()
+                                .background(Color.lightBlue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+
+                        NavigationLink(destination: EventView()) {
+                            Text("Go to an Event")
+                                .padding()
+                                .background(Color.lightBlue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
                     .background(Color.lightBlue)
-                    .foregroundColor(.white)
                     .cornerRadius(10)
+
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            
-            NavigationLink(destination: EventView()) {
-                Text("Go to an Event")
-                    .padding()
-                    .background(Color.lightBlue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarHidden(true)
         }
-        .background(Color.lightBlue)
-        .cornerRadius(10)
-        
-        Spacer()
     }
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .navigationBarTitle("", displayMode: .inline)
-    .navigationBarHidden(true)
 }
-}
-}
-extension Color {
-static let lightBlue = Color(red: 103/255, green: 216/255, blue: 255/255)
-}
-
 
 
 
