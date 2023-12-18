ac88d5a9195d1a310d6bff399597ffc590e01ad6
Tirth Patel <Tirthp0112@gmail.com>
2023-12-18 10:41:39 -0600

    wd

--- EventView.swift
+++ EventView.swift
@@ -2,51 +2,11 @@ import SwiftUI
 
 struct EventView: View {
     var body: some View {
-        ZStack {
-            Color.lightBlue
-                .ignoresSafeArea()
-            
-            VStack {
-                Spacer()
-                
-                VStack {
-                    
-                    
-                    
-                        Text("TEAM MADDUX")
-                        .font(.custom("Georgia", size: 32))
-                        .fontWeight(.bold)
-                        .padding()
-                        .cornerRadius(10)
-                        .foregroundColor(Color.black)
-                    
-                    NavigationLink(destination: AboutView()) {
-                        Text("Maddux Albert Joseph - Life Story")
-                            .padding()
-                            .background(Color.lightBlue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    
-                    NavigationLink(destination: EventView()) {
-                        Text("Go to an Event")
-                            .padding()
-                            .background(Color.lightBlue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                }
-                .background(Color.lightBlue)
-                .cornerRadius(10)
-                
-                Spacer()
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .navigationBarTitle("", displayMode: .inline)
-            .navigationBarHidden(true)
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, worldff!")
         }
     }
-}
-extension Color {
-static let lightBlue = Color(red: 103/255, green: 216/255, blue: 255/255)
 }
