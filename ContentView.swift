7b76895eeac5d706c73cfdc24c2475496a27554c
Brock Wrede <bee15dub@gmail.com>
2023-12-18 10:17:04 -0600

    second commit

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, worldfffffff!")
+            Text("Hello, worldff!")
         }
     }
 }
