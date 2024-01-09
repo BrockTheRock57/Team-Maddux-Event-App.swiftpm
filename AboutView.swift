4442b51272d21a744bbfabccd1ca5a5bf60f5b59
Tirth Patel <Tirthp0112@gmail.com>
2024-01-09 10:28:16 -0600

    swsw

--- AboutView.swift
+++ AboutView.swift
@@ -1,21 +1,12 @@
-//
-//  AboutView.swift
-//  Team Maddux Event App
-//
-//  Created by Brock R. Wrede on 12/18/23.
-//
-
 import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-
-struct AboutView_Previews: PreviewProvider {
-    static var previews: some View {
-        AboutView()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hi Im Maddux!")
+        }
     }
 }
