d117601fe32949c7a3e390200a10c43277a54f32
Brock Wrede <bee15dub@gmail.com>
2024-01-18 11:02:26 -0600

    added file

--- EventView.swift
+++ EventView.swift
@@ -1,12 +1,42 @@
 import SwiftUI
 
 struct EventView: View {
+    
+//    @ObservableObject var event = 
+    
+    
+    
+    
+    
+    
+    
+    
+    
     var body: some View {
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, worldfff!")
         }
+
+        
+        
     }
-}
+    }
+
+
+        
+        struct EventView_Previews: PreviewProvider {
+            static var previews: some View {
+                EventView()
+            }
+        }
+    
+
