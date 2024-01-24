7209e7a6c55a748dc9a84104e21de6972dd034ab
Brock Wrede <bee15dub@gmail.com>
2024-01-24 10:56:47 -0600

    started event view

--- EventView.swift
+++ EventView.swift
@@ -1,35 +1,45 @@
 import SwiftUI
 
 struct EventView: View {
+    let events = ["Apple Picking", "Tree PLanting", "Hockey Game"]
+    let photos = ["photo1", "photo2", "photo3"]
+    let photoData = ["Data for Photo 1", "Data for Photo 2", "Data for Photo 3"]
     
-//    @ObservableObject var event = 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    var body: some View {
-      
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        VStack {
-        }
 
-        
-        
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                ForEach(0..<events.count) { index in
+                    PhotoView(photo: photos[index], event: events[index], data: photoData[index])
+                }
+            }
+            .padding()
+        }
     }
+}
+
+struct PhotoView: View {
+    let photo: String
+    let event: String
+    let data: String
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            
+            Text(event)
+                .font(.title)
+            
+            Image(photo)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
+
+            Text(data)
+                .font(.headline)
+                .foregroundColor(.blue)
+        }
     }
+}
 
 
         
