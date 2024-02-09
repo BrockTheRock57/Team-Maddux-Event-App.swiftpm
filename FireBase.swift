6f297de5335fe44cea8bb308adb91269e369413a
Luke Pasika <Lukepasika@gmail.com>
2024-02-09 10:52:39 -0600

    ghghgh

---
+++ FireBase.swift
@@ -0,0 +1,18 @@
+//
+//  FireBase.swift
+//  Team Maddux Event App
+//
+//  Created by Lukian S. Pasika on 2/9/24.
+//
+
+import SwiftUI
+
+struct FireBase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FireBase()
+}
