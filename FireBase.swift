2e64d52fe2c3b855cc461d30c7a6b3167daad174
Luke Pasika <Lukepasika@gmail.com>
2024-02-15 10:06:58 -0600

    John Joseph

--- FireBase.swift
+++ FireBase.swift
@@ -4,15 +4,47 @@
 //
 //  Created by Lukian S. Pasika on 2/9/24.
 //
-
+import Firebase
 import SwiftUI
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
-struct FireBase: View {
+struct ContentView: View {
+    @State private var users = [User]()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(users, id: \.id) { user in
+            Text(user.name)
+        }
+        .onAppear {
+            fetchData()
+        }
+    }
+
+    func fetchData() {
+        let db = Firestore.firestore()
+        db.collection("users").getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                if let snapshot = snapshot {
+                    self.users = snapshot.documents.compactMap { document in
+                        do {
+                            let user = try document.data(as: User.self)
+                            return user
+                        } catch {
+                            print("Error decoding user: \(error)")
+                            return nil
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    FireBase()
+struct User: Identifiable, Codable {
+    var id: String
+    var name: String
+    // Add other properties as needed
 }
